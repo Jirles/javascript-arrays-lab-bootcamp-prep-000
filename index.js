066ed63a52305d08a088d8fa27e0a9e5fe442eeb
Emily Jirles <emily.jirles@gmail.com>
2018-02-03 15:09:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  //adds kitten to end, returns new array
+  var newArray = kittens.push(name)
+  return newArray
+}
+
+function prependKitten(name){
+  //adds kitten to beginning, returns new array
+  var newArray = kittens.unshift(name)
+  return newArray
+}
+
+function removeLastKitten(){
+  //removes last kitten, returns new array
+  
+}
+
+function removeFirstKitten(){
+  //removes first kitten, returns new array 
+}
